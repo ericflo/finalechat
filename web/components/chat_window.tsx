3e4b327883bfc4a1c41e9b3a823b0db4fb4147e1
Eric Florenzano <floguy@gmail.com>
2023-11-18 12:59:46 -0800

    Simulate data fetch

--- web/components/chat_window.tsx
+++ web/components/chat_window.tsx
@@ -1,13 +1,28 @@
 "use client";
 // Code Context: React TypeScript with bootstrap 5.3.2 and bootstrap-icons injected
 
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback, useRef, use } from "react";
 import ChatMessage from "./chat_message";
 
 export type Message = {
   sender: string;
   content: string;
 };
+
+async function fetchMessages(): Promise<Message[]> {
+  // Replace with actual data fetching logic
+  return [
+    { sender: "User", content: "Hello!" },
+    { sender: "Agent", content: "Hi, how can I help you?" },
+    { sender: "User", content: "I have a question about my order." },
+    { sender: "Agent", content: "Sure, what's your order number?" },
+    { sender: "User", content: "123456789" },
+    { sender: "Agent", content: "Thanks, let me check on that." },
+    { sender: "Agent", content: "It looks like your order is on the way!" },
+    { sender: "Agent", content: "It should arrive by tomorrow." },
+    { sender: "User", content: "Great, thanks!" },
+  ];
+}
 
 // This component is the main chat window that displays the messages between the user and
 // an assistant. It should produce a center column that displays the messages, with a small left
@@ -21,6 +36,15 @@ export default function ChatWindow() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [newMessage, setNewMessage] = useState<string>("");
   const chatHistoryRef = useRef<HTMLDivElement>(null);
+
+  const loadInitialMessages = useCallback(async () => {
+    // Replace with actual data fetching logic
+    const newMessages = await fetchMessages();
+    setMessages(newMessages);
+  }, []);
+  useEffect(() => {
+    loadInitialMessages();
+  }, [loadInitialMessages]);
 
   // Add a new message to the chat
   const addMessage = useCallback((content: string) => {
