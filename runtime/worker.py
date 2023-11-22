54810b1290bbd5b6c3cec23006de216078ca13ea
Eric Florenzano <floguy@gmail.com>
2023-11-22 00:27:11 -0800

    Attempt to rewrite worker

--- runtime/worker.py
+++ runtime/worker.py
@@ -2,65 +2,36 @@ import datetime
 import time
 import traceback
 import json
+import multiprocessing
 
 from vllm import LLM, SamplingParams
 
 from minichat_convo import get_prompt
 from client import DEFAULT_CLIENT
 
-LLM_INSTANCE = {"inst": None}
-
-
-def get_llm(**kwargs):
-    inst = LLM_INSTANCE["inst"]
-    if inst is not None:
-        return inst
-    LLM_INSTANCE["inst"] = LLM(**kwargs)
-    return LLM_INSTANCE["inst"]
-
 
 def get_sampling_params(data):
-    n = data.get("n", 1)
-    best_of = data.get("best_of", 1)
-    presence_penalty = data.get("presence_penalty", 0.0)
-    frequency_penalty = data.get("frequency_penalty", 0.0)
-    repetition_penalty = data.get("repetition_penalty", 1.0)
-    temperature = data.get("temperature", 0.2)
-    top_p = data.get("top_p", 1.0)
-    top_k = data.get("top_k", -1)
-    min_p = data.get("min_p", 0.0)
-    use_beam_search = data.get("use_beam_search", False)
-    length_penalty = data.get("length_penalty", 1.0)
-    early_stopping = data.get("early_stopping", False)
-    stop = data.get("stop", None)
-    stop_token_ids = data.get("stop_token_ids", None)
-    ignore_eos = data.get("ignore_eos", False)
-    max_tokens = data.get("max_tokens", 1024)
-    logprobs = data.get("logprobs", None)
-    prompt_logprobs = data.get("prompt_logprobs", None)
-    skip_special_tokens = data.get("skip_special_tokens", True)
-    spaces_between_special_tokens = data.get("spaces_between_special_tokens", True)
     return SamplingParams(
-        n=n,
-        best_of=best_of,
-        presence_penalty=presence_penalty,
-        frequency_penalty=frequency_penalty,
-        repetition_penalty=repetition_penalty,
-        temperature=temperature,
-        top_p=top_p,
-        top_k=top_k,
-        min_p=min_p,
-        use_beam_search=use_beam_search,
-        length_penalty=length_penalty,
-        early_stopping=early_stopping,
-        stop=stop,
-        stop_token_ids=stop_token_ids,
-        ignore_eos=ignore_eos,
-        max_tokens=max_tokens,
-        logprobs=logprobs,
-        prompt_logprobs=prompt_logprobs,
-        skip_special_tokens=skip_special_tokens,
-        spaces_between_special_tokens=spaces_between_special_tokens,
+        n=data.get("n", 1),
+        best_of=data.get("best_of", 1),
+        presence_penalty=data.get("presence_penalty", 0.0),
+        frequency_penalty=data.get("frequency_penalty", 0.0),
+        repetition_penalty=data.get("repetition_penalty", 1.0),
+        temperature=data.get("temperature", 0.2),
+        top_p=data.get("top_p", 1.0),
+        top_k=data.get("top_k", -1),
+        min_p=data.get("min_p", 0.0),
+        use_beam_search=data.get("use_beam_search", False),
+        length_penalty=data.get("length_penalty", 1.0),
+        early_stopping=data.get("early_stopping", False),
+        stop=data.get("stop", None),
+        stop_token_ids=data.get("stop_token_ids", None),
+        ignore_eos=data.get("ignore_eos", False),
+        max_tokens=data.get("max_tokens", 1024),
+        logprobs=data.get("logprobs", None),
+        prompt_logprobs=data.get("prompt_logprobs", None),
+        skip_special_tokens=data.get("skip_special_tokens", True),
+        spaces_between_special_tokens=data.get("spaces_between_special_tokens", True),
     )
 
 
@@ -103,81 +74,115 @@ def get_llm_kwargs(model, llm_params):
 
 
 def _sort_chats(chats):
-    return chats.sort(
-        key=lambda c: datetime.datetime.fromisoformat(c["created_timestamp"])
+    return sorted(
+        chats, key=lambda c: datetime.datetime.fromisoformat(c["created_timestamp"])
     )
 
 
-def get_all_chats():
-    all_chats = []
-    more_pages = True
-    page = 1
-    while more_pages:
-        chats = DEFAULT_CLIENT.get_chats(page=page)
-        all_chats.extend(chats["items"])
-        more_pages = chats["page"] < chats["pages"]
-        page += 1
-    _sort_chats(all_chats)
-    return all_chats
-
-
 def _sort_messages(messages):
-    return messages.sort(key=lambda m: datetime.datetime.fromisoformat(m["timestamp"]))
+    return sorted(
+        messages, key=lambda m: datetime.datetime.fromisoformat(m["timestamp"])
+    )
 
 
-def get_all_messages(chat_id):
-    all_messages = []
-    more_pages = True
-    page = 1
-    while more_pages:
-        messages = DEFAULT_CLIENT.get_messages(chat_id, page=page)
-        all_messages.extend(messages["items"])
-        more_pages = messages["page"] < messages["pages"]
-        page += 1
-    _sort_messages(all_messages)
-    return all_messages
-
-
-def process_chat(chat, messages):
+def process_chat(
+    chat_id, model, llm_params, sampling_params_json, task_queue, result_queue
+):
     try:
-        DEFAULT_CLIENT.update_chat(chat["id"], status="processing")
-        llm = get_llm(
-            **get_llm_kwargs(model=chat["model"], llm_params=chat["llm_params"])
-        )
-        sampling_params = get_sampling_params(json.loads(chat["sampling_params"]))
-        prompt = get_prompt(model_name=chat["model"], messages=messages)
-        # print(prompt)
-        response = llm.generate([prompt], sampling_params=sampling_params)[0]
-        # print(response)
-        output = "\n".join([o.text for o in response.outputs])
-        DEFAULT_CLIENT.create_message(chat["id"], output, "assistant")
-    except (KeyboardInterrupt, SystemExit):
-        raise
+        llm = LLM(**get_llm_kwargs(model=model, llm_params=llm_params))
+        sampling_params = get_sampling_params(json.loads(sampling_params_json))
+
+        while True:
+            task = task_queue.get()  # Blocking call
+            if task == "TERMINATE":
+                result_queue.put((chat_id, "terminated"))
+                break  # Gracefully exit the loop for termination
+
+            chat, messages = task
+            if chat["model"] != model:
+                result_queue.put((chat_id, "model_change"))
+                break  # Exit if model has changed
+
+            DEFAULT_CLIENT.update_chat(chat_id, status="processing")
+            prompt = get_prompt(model_name=model, messages=messages)
+            response = llm.generate([prompt], sampling_params=sampling_params)[0]
+            output = "\n".join([o.text for o in response.outputs])
+            DEFAULT_CLIENT.create_message(chat_id, output, "assistant")
+            result_queue.put((chat_id, "success"))
+    except Exception as e:
+        traceback.print_exc()  # Improved error logging
+        result_queue.put((chat_id, "error", str(e)))
     finally:
-        DEFAULT_CLIENT.update_chat(chat["id"], status="idle")
+        DEFAULT_CLIENT.update_chat(chat_id, status="idle")
 
 
 def main():
     round = 0
+    result_queue = multiprocessing.Queue()
+    worker_processes = {}
+    worker_task_queues = {}
+    worker_models = {}  # Track models for each chat
+
     while True:
         round += 1
         print(f"Round {round}")
         try:
-            # for chat in get_all_chats():
-            for chat in _sort_chats(DEFAULT_CLIENT.get_chats()):
-                # messages = get_all_messages(chat["id"])
-                messages = _sort_messages(DEFAULT_CLIENT.get_messages(chat["id"]))
-                if (
-                    messages
-                    and len(messages) > 0
-                    and messages[-1]["sender_type"] == "user"
-                    and chat["status"] == "idle"
-                ):
-                    process_chat(chat, messages)
-        except (KeyboardInterrupt, SystemExit):
-            raise
+            chats = _sort_chats(DEFAULT_CLIENT.get_chats())
+            if chats is not None:
+                for chat in chats:
+                    chat_id = chat["id"]
+                    messages = _sort_messages(DEFAULT_CLIENT.get_messages(chat_id))
+
+                    if (
+                        not messages
+                        or messages[-1]["sender_type"] != "user"
+                        or chat["status"] != "idle"
+                    ):
+                        continue
+
+                    current_model = chat["model"]
+                    if (
+                        chat_id not in worker_processes
+                        or worker_models.get(chat_id) != current_model
+                    ):
+                        if chat_id in worker_processes:
+                            worker_task_queues[chat_id].put("TERMINATE")
+                            worker_processes[chat_id].join()
+                            del worker_processes[chat_id]
+                            del worker_task_queues[chat_id]
+
+                        task_queue = multiprocessing.Queue()
+                        p = multiprocessing.Process(
+                            target=process_chat,
+                            args=(
+                                chat_id,
+                                current_model,
+                                chat["llm_params"],
+                                chat["sampling_params"],
+                                task_queue,
+                                result_queue,
+                            ),
+                        )
+                        p.start()
+                        worker_processes[chat_id] = p
+                        worker_task_queues[chat_id] = task_queue
+                        worker_models[chat_id] = current_model
+
+                    worker_task_queues[chat_id].put((chat, messages))
+
         except Exception as e:
             traceback.print_exception(e)
+
+        # Handling results from subprocesses
+        while not result_queue.empty():
+            chat_id, status, error_message = result_queue.get()
+            if status == "error":
+                print(f"Error processing chat {chat_id}: {error_message}")
+            elif status == "terminated":
+                print(f"Chat {chat_id} process terminated.")
+            elif status == "model_change":
+                print(f"Model change detected for chat {chat_id}.")
+
         time.sleep(1.0)
 
 
