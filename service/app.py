8a81fc2e485b6c4956b9081140dee3478338bdb5
Eric Florenzano <floguy@gmail.com>
2023-11-24 03:40:37 -0800

    Try checkpw

--- service/app.py
+++ service/app.py
@@ -43,9 +43,9 @@ class User(db.Model):
         ).decode("utf-8")
 
     def check_password(self, password):
-        return bcrypt.hashpw(
+        return bcrypt.checkpw(
             password.encode("utf-8"), self.password_hash.encode("utf-8")
-        ) == self.password_hash.encode("utf-8")
+        )
 
 
 class Chat(db.Model):
