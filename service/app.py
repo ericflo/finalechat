308e5a0953acf76f1f6eac4b28ed9e42def46c2b
Eric Florenzano <floguy@gmail.com>
2023-11-24 03:31:18 -0800

    Fixes

--- service/app.py
+++ service/app.py
@@ -38,10 +38,14 @@ class User(db.Model):
         }
 
     def set_password(self, password):
-        self.password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+        self.password_hash = bcrypt.hashpw(
+            password.encode("utf-8"), bcrypt.gensalt()
+        ).decode("utf-8")
 
     def check_password(self, password):
-        return bcrypt.checkpw(password.encode("utf-8"), self.password_hash)
+        return bcrypt.hashpw(
+            password.encode("utf-8"), self.password_hash.encode("utf-8")
+        ) == self.password_hash.encode("utf-8")
 
 
 class Chat(db.Model):
